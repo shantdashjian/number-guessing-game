347d136221035a8ec937ae8452178a4ea31fef2b
Shaun Dashjian <shaundashjian@yahoo.com>
2020-09-10 15:05:21 -0700

    Add 10 turns limit

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,11 @@
 // guess a number
 let answer = Math.floor((Math.random() * (100 - 0)) + 1);
+// add ability for button to check guess against number
+const guessButton = document.querySelector('#guess-button');
 console.log(answer);
 let previousGuesses = [];
-// add ability for button to check guess against number
+const MAX_GUESSES = 10;
+let guessesLeft = MAX_GUESSES;
 //    and give feedback
 function checkGuess() {
     const guessInput = document.querySelector('#guess-input');
@@ -11,18 +14,29 @@ function checkGuess() {
     guessInput.value = '';
     const resultPara = document.querySelector('#result');
     let resultText;
-    if (guessValue === answer) {
-        resultText = 'Congratulations! You win!';
-    } else if (guessValue < answer) {
-        resultText = 'Too low';
-    } else if (guessValue > answer) {
-        resultText = 'Too high';
-    }
-    resultPara.textContent = resultText;
+    guessesLeft--;
     const previousGuessesPara = document.querySelector('#previous-guesses');
     const previousGuessesText = previousGuesses.join(' ');
-    previousGuessesPara.textContent = `Previous guesses: ${previousGuessesText}`;
-}
 
-document.querySelector('#guess-button').onclick = checkGuess;
+    previousGuessesPara.textContent = `Previous guesses: ${previousGuessesText}`;
+    if (guessValue === answer) {
+        resultText = 'Congratulations! You win!';
+        guessInput.setAttribute('disabled', 'true');
+        guessButton.setAttribute('disabled', 'true');
+    } else if (guessesLeft > 0) {
+        if (guessValue < answer) {
+            resultText = 'Too low';
+        } else if (guessValue > answer) {
+            resultText = 'Too high';
+        }
+    } else {
+        resultText = '!!!GAME OVER!!!';
+        guessInput.setAttribute('disabled', 'true');
+        guessButton.setAttribute('disabled', 'true');
+    }
+
+    resultPara.textContent = resultText;
+
+}
+guessButton.onclick = checkGuess;
 
